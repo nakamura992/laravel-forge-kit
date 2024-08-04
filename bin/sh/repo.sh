dd6e431bf91ded1364ed4b68bb6d7a627c24589e
nakamura992 <125175223+nakamura992@users.noreply.github.com>
2024-08-04 14:15:46 +0900

    リポジトリ用shファイルを追加

---
+++ bin/sh/repo.sh
@@ -0,0 +1,30 @@
+# コンテナをビルドして起動
+docker-compose -f docker-compose.init.yml up -d --build
+
+# composer install
+echo "Running composer install..."
+docker-compose exec -T app bash -c "if [ -f composer.json ]; then composer install; fi"
+
+# npm install
+echo "Running npm install..."
+docker-compose exec -T app bash -c "if [ -f package.json ]; then npm install; fi"
+
+# コンテナを削除
+echo "exec command: docker-compose down"
+docker-compose down
+
+# コンテナを通常通りのymlでビルド
+echo "exec command: docker-compose up -d --build"
+docker-compose up -d --build
+
+# ホストのvendorディレクトリをコンテナのappにコピー
+echo "exec command: docker-compose cp .src/vendor app:/var/www/html"
+docker-compose cp ./src/vendor app:/var/www/html
+
+# ホストのnode_modulesディレクトリをコンテナのappにコピー
+echo "exec command: docker-compose cp .src/node_modules app:/var/www/html"
+docker-compose cp ./src/node_modules app:/var/www/html
+
+# ホストのstorageディレクトリをコンテナのappにコピー
+echo "exec command: docker-compose cp .src/storage app:/var/www/html"
+docker-compose cp ./src/storage app:/var/www/html
